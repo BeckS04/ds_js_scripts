f216f8d72f246b1ed3e8588f152e42738b055b9f
BeckS04 <113600976+BeckS04@users.noreply.github.com>
2026-05-28 11:04:56 +0200

    Fix optionGroup and update default options

--- farm_god.js
+++ farm_god.js
@@ -490,7 +490,7 @@ window.FarmGod.Main = (function (Library, Translation) {
                 UI.Throbber[0].outerHTML + '<br><br>'
               );
               getData(
-                ,
+                optionGroup,
                 optionNewbarbs,
                 optionLosses
               ).then((data) => {
@@ -568,9 +568,9 @@ window.FarmGod.Main = (function (Library, Translation) {
 
   const buildOptions = function () {
     let options = JSON.parse(localStorage.getItem('farmGod_options')) || {
-      : 0,
-      optionDistance: 25,
-      optionTime: 10,
+      optionGroup: 26592,
+      optionDistance: 17,
+      optionTime: 1,
       optionLosses: false,
       optionMaxloot: true,
       optionNewbarbs: true,
@@ -599,11 +599,13 @@ window.FarmGod.Main = (function (Library, Translation) {
             : ``
           }
                 <div style="width:90%;margin:auto;background: url(\'graphic/index/main_bg.jpg\') 100% 0% #E3D5B3;border: 1px solid #7D510F;border-collapse: separate !important;border-spacing: 0px !important;"><table class="vis" style="width:100%;text-align:left;font-size:11px;">
-                  <tr><td>${t.options.group}</td><td>"26592"</td></tr>
+                  <tr><td>${t.options.group}</td><td>${groupSelect}</td></tr>
                   <tr><td>${t.options.distance
-          }</td><td><input type="text" size="5" class="optionDistance" value="17"></td></tr>
+          }</td><td><input type="text" size="5" class="optionDistance" value="${options.optionDistance
+          }"></td></tr>
                   <tr><td>${t.options.time
-          }</td><td><input type="text" size="5" class="optionTime" value="1"></td></tr>
+          }</td><td><input type="text" size="5" class="optionTime" value="${options.optionTime
+          }"></td></tr>
                   <tr><td>${t.options.losses
           }</td><td><input type="checkbox" class="optionLosses" ${options.optionLosses ? 'checked' : ''
           }></td></tr>
