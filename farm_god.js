a6bb10bfc9240cb86a86a5cbb90861573d5b5f00
BeckS04 <113600976+BeckS04@users.noreply.github.com>
2026-05-28 11:01:11 +0200

    Fix syntax errors and update optionGroup handling

--- farm_god.js
+++ farm_god.js
@@ -490,7 +490,7 @@ window.FarmGod.Main = (function (Library, Translation) {
                 UI.Throbber[0].outerHTML + '<br><br>'
               );
               getData(
-                optionGroup,
+                ,
                 optionNewbarbs,
                 optionLosses
               ).then((data) => {
@@ -568,7 +568,7 @@ window.FarmGod.Main = (function (Library, Translation) {
 
   const buildOptions = function () {
     let options = JSON.parse(localStorage.getItem('farmGod_options')) || {
-      optionGroup: 0,
+      : 0,
       optionDistance: 25,
       optionTime: 10,
       optionLosses: false,
@@ -599,7 +599,7 @@ window.FarmGod.Main = (function (Library, Translation) {
             : ``
           }
                 <div style="width:90%;margin:auto;background: url(\'graphic/index/main_bg.jpg\') 100% 0% #E3D5B3;border: 1px solid #7D510F;border-collapse: separate !important;border-spacing: 0px !important;"><table class="vis" style="width:100%;text-align:left;font-size:11px;">
-                  <tr><td>${t.options.group}</td><td>${groupSelect}</td></tr>
+                  <tr><td>${t.options.group}</td><td>"26592"</td></tr>
                   <tr><td>${t.options.distance
           }</td><td><input type="text" size="5" class="optionDistance" value="17"></td></tr>
                   <tr><td>${t.options.time
