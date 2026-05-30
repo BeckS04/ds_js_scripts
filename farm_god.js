bd4763ec38549a5cc8d0626c9bdb5468858401aa
BeckS04 <113600976+BeckS04@users.noreply.github.com>
2026-05-30 22:01:32 +0200

    Add dropdown for option group selection

--- farm_god.js
+++ farm_god.js
@@ -599,7 +599,14 @@ window.FarmGod.Main = (function (Library, Translation) {
             : ``
           }
                 <div style="width:90%;margin:auto;background: url(\'graphic/index/main_bg.jpg\') 100% 0% #E3D5B3;border: 1px solid #7D510F;border-collapse: separate !important;border-spacing: 0px !important;"><table class="vis" style="width:100%;text-align:left;font-size:11px;">
-                  <tr><td>${t.options.group}</td><td>DarfFarmen</td></tr>
+                  <tr>
+                    <td>${t.options.group}</td>
+                    <td>
+                      <select class="optionGroup">
+                        <option value="26592" selected>DarfFarmen</option>
+                      </select>
+                    </td>
+                  </tr>
                   <tr><td>${t.options.distance
           }</td><td><input type="text" size="5" class="optionDistance" value="17"></td></tr>
                   <tr><td>${t.options.time
